af2de4d6e340f16eb460dab593ceed948692d9bb
Martin9507 <martin_larame@fet.edu.co>
2021-06-22 19:59:00 -0500

    ConvocatoriaFET

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {Text, View, StyleSheet, Image, Button, Alert} from 'react-native'
+import LogoImage from './assets/logo.png'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = ()=>{
+  return(  
+    <View style={styles.contenido}>
+      <Image
+      // source = {{uri:'appcel/assets/logo.png'}}
+      source = {LogoImage}
+      style={styles.imageStyle}
+      />
+      {/* <Text style={styles.title}>
+        JJ BURGUER
+      </Text> */}
+      <Button 
+      onPress= {()=>
+      Alert.alert("Ok")}
+      title= "Entrar"
+      />
+     
     </View>
-  );
-}
+  )}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    contenido:{flex:1, justifyContent:'center', alignItems:'center'},
+    // title: {fontSize:30},
+    imageStyle: {height:175, width:300, borderRadius:90}
+  })
+export default App;
